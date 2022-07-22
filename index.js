df67225f1cdb0ee6ce334366927a844143949aea
Kyle Tillson <kyletillson004@gmail.com>
2022-07-22 09:42:03 -0600

    commit tree 0899c4ef79d8908054739101c82902ea23bc1586

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+superbowlWin = (record) => {
+    const result = record.find( record => record.result === 'W' );
+    return !! result ? result.year : undefined;
+    // const sadReality = record.find( record => record.result === 'W' );
+    // return sadReality.year undefined;
+};
+
